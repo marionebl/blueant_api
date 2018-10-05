3a38054b4cb881208ca9194ff1b7087e6eff151c
Mario Nebl <hello@mario-nebl.de>
2018-10-05 21:10:20 +0200

    Introduce flasgger

--- bluecal/bluecal.py
+++ bluecal/bluecal.py
@@ -1,14 +1,50 @@
 from flask import Flask, request, jsonify
+from flasgger import Swagger
 from .client.client import Client
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+app.config['SWAGGER'] = {
+    'title': 'BlueCal API',
+    'uiversion': 3
+}
+
+swagger = Swagger(app)
 
 @app.route("/login", methods=["POST"])
 def login():
+    """Log in to BlueAnt by providing username and password.
+    Will yield a session id that can be used to authenticate other requests.
+    ---
+    parameters:
+      - name: username
+        in: formData
+        type: string
+        required: true
+      - name: password
+        in: formData
+        type: string
+        required: true
+        format: password
+    definitions:
+      Session:
+        type: object
+        properties:
+          sessionID:
+            type: string
+          persionID:
+            type: string
+    responses:
+      200:
+        description: A session consisting of an ID and a user reference
+        schema:
+          $ref: '#/definitions/Session'
+        examples:
+          Session: { "personID": 69689598, "sessionID": "UXx6XRaM2qmUQPqcvygl" }
+    """
     json = request.get_json()
-    data = request.form if json is None else json;
+    data = request.form if json is None else json
 
     username = data.get("username")
     password = data.get("password")
