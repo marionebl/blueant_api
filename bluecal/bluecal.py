3a0c7ba98aac94f6253fedaca32ea22a7adf4213
Mario Nebl <hello@mario-nebl.de>
2018-10-06 15:56:39 +0200

    Use swagger securityDefinition

--- bluecal/bluecal.py
+++ bluecal/bluecal.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, abort, Response
-from flasgger import Swagger,swag_from
+from flasgger import Swagger, swag_from
 from zeep.exceptions import Fault
 from .client.client import Client
 from .routes.activities import activities
@@ -9,11 +9,24 @@ from .routes.project_tasks import project_tasks
 from .exceptions import InternalServerError, Unauthorized
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object("config")
 
 app.config['SWAGGER'] = {
-    'title': 'BlueCal API',
-    'uiversion': 3
+    "title": "BlueCal API",
+    "version": "1.0.0",
+    "uiversion": 3,
+    "securityDefinitions": {
+      "SessionAuth": {
+          "type": "apiKey",
+          "name": "X-Session-ID",
+          "in": "header"
+      },
+      "PersonAuth": {
+          "type": "apiKey",
+          "name": "X-Person-ID",
+          "in": "header"
+      }
+    }
 }
 
 swagger = Swagger(app)
@@ -22,22 +35,25 @@ swagger = Swagger(app)
 @app.route("/auth/login", methods=["POST"])
 @swag_from("routes/login.yml")
 def login_route():
-  return login()
+    return login()
+
 
 @app.route("/activities", methods=["GET"])
 @swag_from("routes/activities.yml")
 def activities_route():
-  return activities()
+    return activities()
+
 
 @app.route("/projects", methods=["GET"])
 @swag_from("routes/projects.yml")
 def projects_route():
-  return projects()
+    return projects()
+
 
 @app.route("/project/<string:project_id>/tasks", methods=["GET"])
 @swag_from("routes/project_tasks.yml")
 def project_tasks_route(project_id):
-  return project_tasks(project_id)
+    return project_tasks(project_id)
 
 
 @app.errorhandler(InternalServerError)
